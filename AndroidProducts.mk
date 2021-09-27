7c2daaaf5bc2b19ac4fa7bb72cdb1c508ec315dc
amier_san09 <73158213+MhmmdAmier@users.noreply.github.com>
2021-09-27 23:47:11 +0700

    ulysse: AOSP Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ulysse.mk
+    $(LOCAL_DIR)/aosp_ulysse.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ulysse-user \
-    lineage_ulysse-userdebug \
-    lineage_ulysse-eng
+    aosp_ulysse-user \
+    aosp_ulysse-userdebug \
+    aosp_ulysse-eng
