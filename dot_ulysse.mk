6759be5d20f8efc6bbb4e16ffa6d9f78f05153b7
Jagaddhita Jalu <88934027+jagaddhita@users.noreply.github.com>
2021-09-30 03:51:50 -0700

    Update and rename aosp_ulysse.mk to dot_ulysse.mk

--- dot_ulysse.mk
+++ dot_ulysse.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Dot stuff.
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 # Inherit from ulysse device
 $(call inherit-product, device/xiaomi/ulysse/device.mk)
@@ -25,7 +25,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ulysse
-PRODUCT_NAME := aosp_ulysse
+PRODUCT_NAME := dot_ulysse
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 5A / Y1 
